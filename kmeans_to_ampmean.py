db58fde00781784059b149e089f0298fda0439a9
T-S1 <yama153shou@gmail.com>
2022-08-10 20:05:33 +0900

    wrote till kmeans

--- kmeans_to_ampmean.py
+++ kmeans_to_ampmean.py
@@ -1,30 +1,67 @@
 """モジュールのインポート"""
+import pdb; pdb.set_trace()
 import numpy as np
 from scipy import signal
+from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
-"""データの読み込み"""
-data = np.loadtxt(f"./data/normal/0000.csv", delimiter=",")
-t = data[:, 0]
-x = data[:, 1]
+n_data = 50
+ms = np.zeros(n_data)
 
-x_abs = np.abs(x)
+for i in range(n_data):
+    """データの読み込み"""
+    data = np.loadtxt(f"./data/example/{i:04}.csv", delimiter=",")
+    t = data[:, 0]
+    x = data[:, 1]
 
-"""代表値の抽出
-1. 極値の検出
-2. 振幅の平均値の算出
-"""
-# 1
-peaks, _ = signal.find_peaks(x_abs, prominence=0.5, width=5)
-# print(peaks)
+    x_abs = np.abs(x)
 
-fig = plt.figure()
-plt.plot(t, x)
-plt.plot(t[peaks], x[peaks], "x")
-plt.show()
+    """代表値の抽出
+    1. 極値の検出
+    2. 振幅の平均値の算出
+    """
+    # 1
+    peaks, _ = signal.find_peaks(x_abs, prominence=0.3, width=5)
+    # print(peaks)
 
-# 2
-mean_amp = np.mean(x_abs[peaks])
-print(f"振幅平均値: {mean_amp}")
+    # fig = plt.figure()
+    # plt.plot(t, x)
+    # plt.plot(t[peaks], x[peaks], "x")
+    # plt.show()
+
+    # 2
+    m_amp = np.mean(x_abs[peaks])
+    # print(f"振幅平均値: {mean_amp}")
+
+    ms[i] = m_amp
 
 """k-meansの実行"""
+n_clusters = 3
+colors = ["r", "g", "b"]
+
+ms = ms.reshape(-1, 1)
+
+kmeans = KMeans(
+    n_clusters,
+    n_init=10,
+    max_iter=300,
+    random_state=100
+).fit(ms)
+centers = kmeans.cluster_centers_
+labels = kmeans.predict(ms)
+
+fig = plt.figure()
+for i in range(n_clusters):
+    idxs = np.arange(n_data)[labels == i]
+    plt.scatter(idxs, ms[idxs], c=colors[i])
+    plt.hlines(centers[i], xmin=0, xmax=n_data)
+plt.show()
+
+"""リアルタイムな異常検知"""
+np.random.seed(100)
+n_iter = 20
+
+
+
+for i in range(n_iter):
+    x_new = np.loadtxt
