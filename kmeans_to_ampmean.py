20b4e8e656229dd2485e971803f6382a1f5af23c
T-S1 <yama153shou@gmail.com>
2022-08-10 18:05:24 +0900

    writing main

--- kmeans_to_ampmean.py
+++ kmeans_to_ampmean.py
@@ -1,19 +1,30 @@
+"""モジュールのインポート"""
 import numpy as np
 from scipy import signal
 import matplotlib.pyplot as plt
 
-# データの読み込み
-x = np.loadtxt(f"./data/temp/00000.txt")
+"""データの読み込み"""
+data = np.loadtxt(f"./data/normal/0000.csv", delimiter=",")
+t = data[:, 0]
+x = data[:, 1]
 
 x_abs = np.abs(x)
 
-peaks, _ = signal.find_peaks(x_abs, prominence=0.8, width=5)
+"""代表値の抽出
+1. 極値の検出
+2. 振幅の平均値の算出
+"""
+# 1
+peaks, _ = signal.find_peaks(x_abs, prominence=0.5, width=5)
 # print(peaks)
 
-
-
 fig = plt.figure()
-plt.plot(x)
-
-plt.plot(peaks, x[peaks], "x")
+plt.plot(t, x)
+plt.plot(t[peaks], x[peaks], "x")
 plt.show()
+
+# 2
+mean_amp = np.mean(x_abs[peaks])
+print(f"振幅平均値: {mean_amp}")
+
+"""k-meansの実行"""
