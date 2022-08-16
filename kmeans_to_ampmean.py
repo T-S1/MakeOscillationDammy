0d70bf1fbe9da9d780a97f47c30ea0e82152e32f
T-S1 <yama153shou@gmail.com>
2022-08-17 00:41:53 +0900

    tested

--- kmeans_to_ampmean.py
+++ kmeans_to_ampmean.py
@@ -1,37 +1,43 @@
 """モジュールのインポート"""
-# import pdb; pdb.set_trace()
 import numpy as np
 from scipy import signal
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
+"""データの読み込み"""
+data = np.loadtxt("./data/example1/0000.csv", delimiter=",")
+t = data[:, 0]
+x = data[:, 1]
+
+"""代表値の抽出
+1. 極値の検出
+2. 振幅の平均値の算出
+"""
+# 1
+x_abs = np.abs(x)
+peaks, _ = signal.find_peaks(x_abs, prominence=0.3, width=5)
+
+fig = plt.figure()
+plt.plot(t, x)
+plt.plot(t[peaks], x[peaks], "x")
+plt.show()
+
+# 2
+m_amp = np.mean(x_abs[peaks])
+print(f"振幅平均：{m_amp}")
+
+"""全データの読み込み"""
 n_data = 50
 ms = np.zeros(n_data)
 
 for i in range(n_data):
-    """データの読み込み"""
     data = np.loadtxt(f"./data/example1/{i:04}.csv", delimiter=",")
     t = data[:, 0]
     x = data[:, 1]
 
     x_abs = np.abs(x)
-
-    """代表値の抽出
-    1. 極値の検出
-    2. 振幅の平均値の算出
-    """
-    # 1
     peaks, _ = signal.find_peaks(x_abs, prominence=0.3, width=5)
-    # print(peaks)
-
-    # fig = plt.figure()
-    # plt.plot(t, x)
-    # plt.plot(t[peaks], x[peaks], "x")
-    # plt.show()
-
-    # 2
     m_amp = np.mean(x_abs[peaks])
-    # print(f"振幅平均値: {mean_amp}")
 
     ms[i] = m_amp
 
@@ -61,15 +67,13 @@ plt.show()
 data = np.loadtxt("./data/rt_example1.csv", delimiter=",")
 t = data[:, 0]
 x = data[:, 1]
-n_samples = 4096
 n_window = 256
 width = 500
 interval = 50
-clus_names = ["normal", "abnormal_1", "abnormal_2"]
 
 fig = plt.figure()
 
-for i in range(1, n_samples):
+for i in range(len(x)):
     if i % n_window == n_window - 1:
         i_start = i - n_window + 1
         x_abs = np.abs(x[i_start: i])
@@ -77,11 +81,12 @@ for i in range(1, n_samples):
         m_amp = np.mean(x_abs[peaks])
         label = kmeans.predict([[m_amp]])[0]
         plt.axvspan(t[i_start], t[i], color=colors[label])
-    if i % interval == 0:
-        ileft = max(0, i - width)
-        iright = max(width, i)
-        plt.xlim(t[ileft], t[iright])
-        plt.plot(t[i - interval: i + 1], x[i - interval: i + 1], color='k')
+    if i % interval == interval - 1:
+        i_left = max(0, i - width)
+        i_right = max(width, i)
+        plt.xlim(t[i_left], t[i_right])
+        i_start = i - interval + 1
+        plt.plot(t[i_start: i + 2], x[i_start: i + 2], color='k')
         plt.pause(0.2)
 
 plt.show()
