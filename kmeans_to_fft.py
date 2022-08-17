4c02ec0f756be24f28a07d34b2bed70147afd45f
T-S1 <yama153shou@gmail.com>
2022-08-17 19:29:34 +0900

    tested

--- kmeans_to_fft.py
+++ kmeans_to_fft.py
@@ -6,7 +6,7 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
 """データの読み込み"""
-data = np.loadtxt("./data/example2/0030.csv", delimiter=",")
+data = np.loadtxt("./data/example2/0000.csv", delimiter=",")
 t = data[:, 0]
 x = data[:, 1]
 
@@ -22,12 +22,13 @@ plt.plot(f, spectrum)
 plt.show()
 
 """全データの読み込み"""
-n_data = 100
+n_data = 50
 n_features = 256
 feats = np.zeros((n_data, n_features))
 
 if not os.path.isdir("./figures/fft_example2"):
     os.makedirs("./figures/fft_example2")
+
 fig = plt.figure()
 
 for i in range(n_data):
@@ -41,10 +42,10 @@ for i in range(n_data):
     f = np.arange(len(t)) * f_0
 
     # feats[i, :] = spectrum / np.amax(spectrum)
-    feat = spectrum / (np.sum(spectrum)*f_0)
+    feat = spectrum / np.sum(spectrum)
 
-    ave_win_size = 5
-    ave_win = np.ones(ave_win_size) / ave_win_size
+    filter_size = 3
+    ave_win = np.ones(filter_size) / filter_size
     feat = np.convolve(ave_win, feat, "same")
     feats[i, :] = feat
 
@@ -56,12 +57,10 @@ plt.close()
 
 """k-meansの実行"""
 n_clusters = 3
-colors = ["g", "r", "b"]
+colors = ["r", "g", "b"]
 
 kmeans = KMeans(
     n_clusters,
-    n_init=10,
-    max_iter=300,
     random_state=100
 ).fit(feats)
 centers = kmeans.cluster_centers_
@@ -79,35 +78,41 @@ for i in range(n_clusters):
     axs[i].plot(f, centers[i], color=colors[i])
 plt.show()
 
-# fig = plt.figure()
-# for i in range(n_clusters):
-#     plt.plot(f, centers[i], color=colors[i], linewidth=0.5)
-# plt.show()
+"""リアルタイムな異常検知"""
+data = np.loadtxt("./data/rt_example2.csv", delimiter=",")
+t = data[:, 0]
+x = data[:, 1]
+n_window = 256
+width = 500
+interval = 32
 
-# """リアルタイムな異常検知"""
-# data = np.loadtxt("./data/rt_example1.csv", delimiter=",")
-# t = data[:, 0]
-# x = data[:, 1]
-# n_window = 256
-# width = 500
-# interval = 50
+fig = plt.figure()
 
-# fig = plt.figure()
+for i in range(len(x)):
+    if i % n_window == n_window - 1:
+        i_start = i - n_window + 1
+        x_win = x[i_start: i + 1]
 
-# for i in range(len(x)):
-#     if i % n_window == n_window - 1:
-#         i_start = i - n_window + 1
-#         x_abs = np.abs(x[i_start: i])
-#         peaks, _ = signal.find_peaks(x_abs, prominence=0.3, width=5)
-#         m_amp = np.mean(x_abs[peaks])
-#         label = kmeans.predict([[m_amp]])[0]
-#         plt.axvspan(t[i_start], t[i], color=colors[label])
-#     if i % interval == interval - 1:
-#         i_left = max(0, i - width)
-#         i_right = max(width, i)
-#         plt.xlim(t[i_left], t[i_right])
-#         i_start = i - interval + 1
-#         plt.plot(t[i_start: i + 2], x[i_start: i + 2], color='k')
-#         plt.pause(0.2)
+        fourier = np.fft.fft(x_win)
+        spectrum = fourier.real**2 + fourier.imag**2
+        f = np.arange(len(x_win)) * f_0
+        feat = spectrum / np.sum(spectrum)
 
-# plt.show()
+        filter_size = 3
+        ave_win = np.ones(filter_size) / filter_size
+        feat = np.convolve(ave_win, feat, "same")
+
+        label = kmeans.predict([feat])[0]
+        plt.axvspan(t[i_start], t[i], color=colors[label])
+
+    if i % interval == interval - 1:
+        i_left = max(0, i - width)
+        i_right = max(width, i)
+        plt.xlim(t[i_left], t[i_right])
+        i_start = i - interval + 1
+        plt.plot(t[i_start: i + 2], x[i_start: i + 2], color='k')
+        plt.pause(0.1)
+
+plt.xlim(t[0], t[-1])
+plt.savefig("./figures/rt_fft.jpg")
+plt.show()
