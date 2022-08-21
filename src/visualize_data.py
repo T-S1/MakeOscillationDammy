b727023cf0552f7c024958a1cf3ca6f5c2c96291
T-S1 <yama153shou@gmail.com>
2022-08-22 00:43:22 +0900

    update

--- src/visualize_data.py
+++ src/visualize_data.py
@@ -1,40 +1,55 @@
-import os
+# import os
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+plt.rcParams['font.family'] = "Meiryo"  # フォントの設定
 
-plt.rcParams['font.family'] = "Meiryo"
-
-if not os.path.isdir("./figures/fft_example2"):
-    os.makedirs("./figures/fft_example2")
+# if not os.path.isdir("./figures/fft_example2"):
+#     os.makedirs("./figures/fft_example2")
 
 
-def show_signal(t, x, name):
-    fig = plt.figure()
-    plt.title(name)
-    plt.xlabel("時刻")
-    plt.ylabel("信号強度")
-    plt.xlim(t[0], t[-1])
-    plt.ylim(-2.5, 2.5)
-    plt.plot(t, x)
+def show_signals(t, x, step=8, n_rows=3, n_cols=2, y_max=2.5):
+    fig, axs = plt.subplots(n_rows, n_cols, sharex=True, sharey=True)   # 複数グラフを表示する準備
+    for i in range(n_rows):
+        for j in range(n_cols):
+            idx = step * (n_cols * i + j)
+            axs[i, j].set_title(f"{idx:04}")    # グラフタイトル
+            axs[i, j].set_xlim(t[idx, 0], t[idx, -1])   # x軸の上下限
+            axs[i, j].set_ylim(-y_max, y_max)           # y軸の上下限
+            axs[i, j].plot(t[idx], x[idx])      # 波形のプロット
+    plt.tight_layout()  # 体裁を整える
+    plt.show()          # 表示
+
+
+def show_peaks(t, x, peaks_list, step=8, n_rows=3, n_cols=2, y_max=2.5):
+    fig, axs = plt.subplots(n_rows, n_cols, sharex=True, sharey=True)
+    for i in range(n_rows):
+        for j in range(n_cols):
+            idx = step * (n_cols * i + j)
+            peaks = peaks_list[idx]
+            axs[i, j].set_title(f"{idx:04}")
+            axs[i, j].set_xlim(t[idx, 0], t[idx, -1])
+            axs[i, j].set_ylim(-y_max, y_max)
+            axs[i, j].plot(t[idx], x[idx])
+            axs[i, j].plot(t[idx, peaks], x[idx, peaks], "x")
+    plt.tight_layout()
     plt.show()
 
 
-def show_peaks(t, x, peaks, name):
+def show_amp_means(amp_means):
+    n_data = len(amp_means)
     fig = plt.figure()
-    plt.title(name)
-    plt.xlabel("時刻")
-    plt.ylabel("信号強度")
-    plt.xlim(t[0], t[-1])
-    plt.ylim(-2.5, 2.5)
-    plt.plot(t, x)
-    plt.plot(t[peaks], x[peaks], "x")
+    plt.title("代表値")
+    plt.xlabel("データ番号")
+    plt.ylabel("平均振幅")
+    idxs = np.arange(n_data)
+    plt.scatter(idxs, amp_means)
     plt.show()
 
 
-def show_amp_clusters(ms, labels, centers, colors=["r", "g", "y"]):
-    n_data = len(ms)
+def show_amp_clusters(amp_means, labels, centers, colors=["r", "g", "y"]):
+    n_data = len(amp_means)
     n_clusters = len(colors)
     fig = plt.figure()
     plt.title("クラスタリング結果")
@@ -42,24 +57,57 @@ def show_amp_clusters(ms, labels, centers, colors=["r", "g", "y"]):
     plt.ylabel("平均振幅")
     for cluster in range(n_clusters):
         idxs = np.arange(n_data)[labels == cluster]
-        plt.scatter(idxs, ms[idxs], c=colors[cluster])
+        plt.scatter(idxs, amp_means[idxs], c=colors[cluster])
         plt.hlines(centers[cluster], xmin=0, xmax=n_data)
     plt.show()
 
 
-def show_spectrum(f, spectrum, y_max=160):
-    fig = plt.figure()
-    plt.ylim(0, y_max)
-    plt.plot(f, spectrum)
+def show_signals_with_cluster(
+    t, x, labels, colors=["r", "g", "y"], step=8, n_rows=3, n_cols=2, y_max=2.5
+):
+    fig, axs = plt.subplots(n_rows, n_cols, sharex=True, sharey=True)
+    for i in range(n_rows):
+        for j in range(n_cols):
+            idx = step * (n_cols * i + j)
+            axs[i, j].set_title(f"{idx:04}")
+            axs[i, j].set_xlim(t[idx, 0], t[idx, -1])
+            axs[i, j].set_ylim(-y_max, y_max)
+            axs[i, j].plot(t[idx], x[idx], color=colors[labels[idx]])
+    plt.tight_layout()
     plt.show()
 
 
-def savefig_spectrum(idx, f, spectrum, y_max=160):
+def show_spectrums(freq, spectrums, step=8, n_rows=3, n_cols=2, y_max=200):
+    fig, axs = plt.subplots(n_rows, n_cols, sharex=True, sharey=True)
+    for i in range(n_rows):
+        for j in range(n_cols):
+            idx = step * (n_cols * i + j)
+            axs[i, j].set_title(f"{idx:04}")
+            axs[i, j].set_xlim(freq[0], freq[-1])
+            axs[i, j].set_ylim(0, y_max)
+            axs[i, j].plot(freq, spectrums[idx])
+    plt.tight_layout()
+    plt.show()
+
+
+# def savefig_spectrums(idx, f, spectrum, y_max=200):
+#     fig = plt.figure()
+#     plt.ylim(0, y_max)
+#     plt.plot(f, spectrum)
+#     plt.savefig(f"./figures/fft_example2/{idx:04}.jpg")
+#     plt.close()
+
+
+def show_all_spectrums(freq, spectrums):
     fig = plt.figure()
-    plt.ylim(0, y_max)
-    plt.plot(f, spectrum)
-    plt.savefig(f"./figures/fft_example2/{idx:04}.jpg")
-    plt.close()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_xlabel("データ番号")
+    ax.set_ylabel("周波数")
+    ax.set_zlabel("パワー")
+    for i in range(len(spectrums)):
+        arr_i = np.ones_like(freq) * i
+        ax.plot(arr_i, freq, spectrums[i, :], color="tab:blue")
+    plt.show()
 
 
 def show_fft_clusters(f, feats, labels, colors=["r", "g", "y"]):
@@ -74,22 +122,24 @@ def show_fft_clusters(f, feats, labels, colors=["r", "g", "y"]):
     plt.show()
 
 
-def show_fft_centers(f, centers, colors=["r", "g", "y"]):
+def show_fft_centers(f, centers, colors=["r", "g", "y"], y_max=200):
     n_clusters = len(centers)
-    fig, axs = plt.subplots(n_clusters, 1)
+    fig, axs = plt.subplots(n_clusters, 1, sharex=True, sharey=True)
+    plt.suptitle("クラスタ中心")
     for i in range(n_clusters):
         axs[i].set_ylabel("パワー")
         axs[i].plot(f, centers[i], color=colors[i])
-    axs[0].set_title("クラスタ中心")
+        axs[i].set_ylim(0, y_max)
     axs[-1].set_xlabel("周波数")
+    plt.tight_layout()
     plt.show()
 
 
 class RT_Drawer():
-    def __init__(self, t, x, width=500, interval=32):
+    def __init__(self, t, x, width=500, interval=32, y_max=3):
 
-        if not os.path.isdir("./figures"):
-            os.makedirs("./figures")
+        # if not os.path.isdir("./figures"):
+        #     os.makedirs("./figures")
 
         self.width = width
         self.interval = interval
@@ -99,6 +149,7 @@ class RT_Drawer():
         self.fig = plt.figure()
         plt.xlabel("時刻(t)")
         plt.ylabel("信号強度(x)")
+        plt.ylim(-y_max, y_max)
 
     def update(self):
         if self.count % self.interval == self.interval - 1:
